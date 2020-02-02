61cd8b18d16af2bc8683b46c276ea8a1065aacea
Howard Yeh <hayeah@gmail.com>
2020-02-02 11:51:08 +0800

    sign message internally

--- pkg/transformer/eth_sign.go
+++ pkg/transformer/eth_sign.go
@@ -1,9 +1,13 @@
 package transformer
 
 import (
-	"encoding/base64"
+	"bytes"
+	"encoding/binary"
 	"encoding/hex"
 
+	"github.com/btcsuite/btcd/btcec"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/pkg/errors"
 	"github.com/qtumproject/janus/pkg/eth"
 	"github.com/qtumproject/janus/pkg/qtum"
 	"github.com/qtumproject/janus/pkg/utils"
@@ -24,22 +28,41 @@ func (p *ProxyETHSign) Request(rawreq *eth.JSONRPCRequest) (interface{}, error) 
 		return nil, err
 	}
 
-	base58Addr, err := p.FromHexAddress(utils.RemoveHexPrefix(req.Account))
+	addr := utils.RemoveHexPrefix(req.Account)
+
+	acc := p.Qtum.Accounts.FindByHexAddress(addr)
+	if acc == nil {
+		return nil, errors.Errorf("No such account: %s", addr)
+	}
+
+	sig, err := signMessage(acc.PrivKey, req.Message)
 	if err != nil {
 		return nil, err
 	}
 
-	sig, err := p.SignMessage(base58Addr, string(req.Message))
-	if err != nil {
-		return nil, err
-	}
+	return eth.SignResponse("0x" + hex.EncodeToString(sig)), nil
+}
 
-	// base64 -> hex
+func signMessage(key *btcec.PrivateKey, msg []byte) ([]byte, error) {
+	msghash := chainhash.DoubleHashB(paddedMessage(msg))
 
-	sigdata, err := base64.StdEncoding.DecodeString(sig)
-	if err != nil {
-		return nil, err
-	}
+	secp256k1 := btcec.S256()
 
-	return eth.SignResponse("0x" + hex.EncodeToString(sigdata)), nil
+	return btcec.SignCompact(secp256k1, key, msghash, true)
+}
+
+var qtumSignMessagePrefix = []byte("\u0015Qtum Signed Message:\n")
+
+func paddedMessage(msg []byte) []byte {
+	var wbuf bytes.Buffer
+
+	wbuf.Write(qtumSignMessagePrefix)
+
+	var msglenbuf [binary.MaxVarintLen64]byte
+	msglen := binary.PutUvarint(msglenbuf[:], uint64(len(msg)))
+
+	wbuf.Write(msglenbuf[:msglen])
+	wbuf.Write(msg)
+
+	return wbuf.Bytes()
 }
