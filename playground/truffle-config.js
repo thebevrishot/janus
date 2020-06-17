ebdd53e17b6747789583c7ff55ee2a9345b3fb95
VoR0220 <catalanor0220@gmail.com>
2020-06-17 14:32:49 -0500

    fix the network object so that it can find different networks
    Signed-off-by: VoR0220 <catalanor0220@gmail.com>

--- playground/truffle-config.js
+++ playground/truffle-config.js
@@ -1,11 +1,13 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  development: {
-    host: '127.0.0.1',
-    port: 23889, // janus QTUM-ETH RPC bridge
-    network_id: '*', // eslint-disable-line camelcase
-    from: '0x7926223070547d2d15b2ef5e7383e541c338ffe9',
-    gasPrice: '0x64', // minimal gas for qtum
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 23889, // janus QTUM-ETH RPC bridge
+      network_id: '*', // eslint-disable-line camelcase
+      from: '0x7926223070547d2d15b2ef5e7383e541c338ffe9',
+      gasPrice: '0x64', // minimal gas for qtum
+    },
   },
 };
