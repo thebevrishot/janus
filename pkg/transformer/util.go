0d29c6a071d273ea0c4790c668fb800e1afbefaa
IntegralTeam M <repo@sfxdx.ru>
2020-12-19 11:17:17 +0700

    Fix

--- pkg/transformer/util.go
+++ pkg/transformer/util.go
@@ -432,7 +432,7 @@ func (p *ProxyETHGetBlockByHash) GetTransactionByHash(hash string, height, posit
 			Fee:               0.0,
 			Confirmations:     0,
 			Blockhash:         "0000000000000000000000000000000000000000000000000000000000000000",
-			Blockindex:        position,
+			Blockindex:        0,
 			Blocktime:         0,
 			Txid:              decodedRawTx.Txid,
 			Time:              0,
