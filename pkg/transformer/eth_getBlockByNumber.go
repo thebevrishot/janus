8b5e3a4930b0b50a42cb12fef419b41e3f07fc85
IntegralTeam <repo@sfxdx.ru>
2021-01-21 12:18:16 +0300

    pkg/transformer/eth_getBlockByNumber.go: small refactor of ProxyETHGetBlockByNumber.Request()

--- pkg/transformer/eth_getBlockByNumber.go
+++ pkg/transformer/eth_getBlockByNumber.go
@@ -15,18 +15,18 @@ func (p *ProxyETHGetBlockByNumber) Method() string {
 	return "eth_getBlockByNumber"
 }
 
-func (p *ProxyETHGetBlockByNumber) Request(rawreq *eth.JSONRPCRequest) (interface{}, error) {
-	var req eth.GetBlockByNumberRequest
-	if err := unmarshalRequest(rawreq.Params, &req); err != nil {
-		return nil, errors.WithMessage(err, "couldn't unmarhsal request")
+func (p *ProxyETHGetBlockByNumber) Request(rpcReq *eth.JSONRPCRequest) (interface{}, error) {
+	req := new(eth.GetBlockByNumberRequest)
+	if err := unmarshalRequest(rpcReq.Params, req); err != nil {
+		return nil, errors.WithMessage(err, "couldn't unmarhsal rpc request")
 	}
-	return p.request(&req)
+	return p.request(req)
 }
 
 func (p *ProxyETHGetBlockByNumber) request(req *eth.GetBlockByNumberRequest) (*eth.GetBlockByNumberResponse, error) {
 	blockNum, err := getBlockNumberByParam(p.Qtum, req.BlockNumber, 0)
 	if err != nil {
-		return nil, errors.WithMessage(err, "couldn't get block number")
+		return nil, errors.WithMessage(err, "couldn't get block number by parameter")
 	}
 	blockHash, err := p.GetBlockHash(blockNum)
 	if err != nil {
