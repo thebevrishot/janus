546ba04953dd87d868b2b4654c40dc4716f7ef60
IntegralTeam <repo@sfxdx.ru>
2021-01-29 12:03:46 +0300

    pkg/transformer/eth_getTransactionByHash.go: add todo at getTransactionByHash() - sometimes 'receive' category is not present at vouts

--- pkg/transformer/eth_getTransactionByHash.go
+++ pkg/transformer/eth_getTransactionByHash.go
@@ -118,7 +118,12 @@ func getTransactionByHash(p *qtum.Qtum, hash string) (*eth.GetTransactionByHashR
 	}
 	ethTx.To, err = findNonContractTxReceiverAddress(qtumDecodedRawTx.Vouts)
 	if err != nil {
-		return nil, errors.WithMessage(err, "couldn't get non contract transaction receiver address")
+		// TODO: discuss, research
+		// ? Some vouts doesn't have `receive` category at all
+		ethTx.To = utils.AddHexPrefix(qtum.ZeroAddress)
+
+		// TODO: uncomment, after todo above will be resolved
+		// return nil, errors.WithMessage(err, "couldn't get non contract transaction receiver address")
 	}
 	// TODO: discuss
 	// ? Does func above return incorrect address for graph-node (len is < 40)
