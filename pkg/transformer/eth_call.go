5da9cde82ff4acb9ec1c35a9e4e9fc4ea06a2c49
ferret-san <diego.ferrer@ufl.edu>
2021-02-23 09:37:07 -0500

    Adding revert reasons message to eth_call

--- pkg/transformer/eth_call.go
+++ pkg/transformer/eth_call.go
@@ -2,13 +2,10 @@ package transformer
 
 import (
 	"fmt"
-	"math/big"
-	"regexp"
 
 	"github.com/qtumproject/janus/pkg/eth"
 	"github.com/qtumproject/janus/pkg/qtum"
 	"github.com/qtumproject/janus/pkg/utils"
-	"github.com/ethereum/go-ethereum/common/hexutil"
 )
 
 // ProxyETHCall implements ETHProxy
@@ -59,77 +56,38 @@ func (p *ProxyETHCall) ToRequest(ethreq *eth.CallRequest) (*qtum.CallContractReq
 		To:   ethreq.To,
 		From: from,
 		Data: ethreq.Data,
-		// TODO: qtum [code: -3] Invalid value for gasLimit (Minimum is: 10000)
-		// Incorrect gas format
-		GasLimit: big.NewInt(10000),
-		//GasLimit: ethreq.Gas.Int,
+		GasLimit: ethreq.Gas.Int,
 	}, nil
 }
 
 func (p *ProxyETHCall) ToResponse(qresp *qtum.CallContractResponse) interface{} {
 	excepted := qresp.ExecutionResult.Excepted
-	data := utils.AddHexPrefix(qresp.ExecutionResult.Output)
+	exceptedMessage := qresp.ExecutionResult.ExceptedMessage
+	
 	if excepted != "None" {
-		//Get transaction: Might not be needed
-		//get code -> data
-		//decode message
 		
-		message := DecodeMessage(data)
-
-		
-
-
-		/*
-		 	code = eth_call({transaction}, blockNumber)
-			message = decodeMessage(code)
-			return eth.JSONRPCError{
-				"message": "VM Exception while processing transaction: revert CapperRole: caller does not have the Capper role",
-				"code": -32000,
-				"data": {
-				"0x90f48d0a98bf9e18bba07a9538da7a1c21fcccab4aaca3afd60b6d0677098117": {
-					"error": "revert",
-					"program_counter": 621,
-					"return": "0x08c379a000000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000030436170706572526f6c653a2063616c6c657220646f6573206e6f742068617665207468652043617070657220726f6c6500000000000000000000000000000000",
-					"reason": "CapperRole: caller does not have the Capper role"
-				},
-				"stack": "o: VM Exception while processing transaction: revert CapperRole: caller does not have the Capper role\n    at Function.o.fromResults (/Users/howard/src/openzeppelin-solidity/node_modules/ganache-cli/build/ganache-core.node.cli.js:10:81931)\n    at /Users/howard/src/openzeppelin-solidity/node_modules/ganache-cli/build/ganache-core.node.cli.js:47:121235\n    at /Users/howard/src/openzeppelin-solidity/node_modules/ganache-cli/build/ganache-core.node.cli.js:61:1853218\n    at /Users/howard/src/openzeppelin-solidity/node_modules/ganache-cli/build/ganache-core.node.cli.js:2:26124\n    at i (/Users/howard/src/openzeppelin-solidity/node_modules/ganache-cli/build/ganache-core.node.cli.js:2:41179)\n    at /Users/howard/src/openzeppelin-solidity/node_modules/ganache-cli/build/ganache-core.node.cli.js:61:1210468\n    at /Users/howard/src/openzeppelin-solidity/node_modules/ganache-cli/build/ganache-core.node.cli.js:2:105466\n    at /Users/howard/src/openzeppelin-solidity/node_modules/ganache-cli/build/ganache-core.node.cli.js:32:392\n    at c (/Users/howard/src/openzeppelin-solidity/node_modules/ganache-cli/build/ganache-core.node.cli.js:32:5407)\n    at /Users/howard/src/openzeppelin-solidity/node_modules/ganache-cli/build/ganache-core.node.cli.js:32:317\n    at /Users/howard/src/openzeppelin-solidity/node_modules/ganache-cli/build/ganache-core.node.cli.js:61:1871644\n    at /Users/howard/src/openzeppelin-solidity/node_modules/ganache-cli/build/ganache-core.node.cli.js:2:23237\n    at o (/Users/howard/src/openzeppelin-solidity/node_modules/ganache-cli/build/ganache-core.node.cli.js:2:26646)\n    at /Users/howard/src/openzeppelin-solidity/node_modules/ganache-cli/build/ganache-core.node.cli.js:2:26124\n    at /Users/howard/src/openzeppelin-solidity/node_modules/ganache-cli/build/ganache-core.node.cli.js:61:1864681\n    at /Users/howard/src/openzeppelin-solidity/node_modules/ganache-cli/build/ganache-core.node.cli.js:61:1862544\n    at /Users/howard/src/openzeppelin-solidity/node_modules/ganache-cli/build/ganache-core.node.cli.js:61:1889757\n    at /Users/howard/src/openzeppelin-solidity/node_modules/ganache-cli/build/ganache-core.node.cli.js:2:23237\n    at o (/Users/howard/src/openzeppelin-solidity/node_modules/ganache-cli/build/ganache-core.node.cli.js:2:26646)\n    at /Users/howard/src/openzeppelin-solidity/node_modules/ganache-cli/build/ganache-core.node.cli.js:2:26124\n    at /Users/howard/src/openzeppelin-solidity/node_modules/ganache-cli/build/ganache-core.node.cli.js:2:5439\n    at FSReqCallback.args [as oncomplete] (fs.js:145:20)",
-				"name": "o"
-				}
+		if exceptedMessage != "" {
+			return &eth.JSONRPCError{
+				Message: fmt.Sprintf("%s", exceptedMessage),
+				Code:    -32000,
 			}
-		*/
+		}
+
+		//This will most likely occur due to OutOfGasIntrinsic
 		return &eth.JSONRPCError{
-			Code:    -32000,
 			Message: fmt.Sprintf("VM exception: %s", excepted),
+			Code:    -32000,
 			// To see how eth_call supports revert reason, see:
 			// https://gist.github.com/hayeah/795bc18a683053218fb3ff5032d31144
 			//
 			// Data: ...
 		}
+		
+		
+		
 	}
 
-	
+	data := utils.AddHexPrefix(qresp.ExecutionResult.Output)
 	qtumresp := eth.CallResponse(data)
 	return &qtumresp
-}
-
-func (p *ProxyETHCall) DecodeMessage(code string) string {
-
-	var codeString string
-
-	const FnSelectorByteLength = 4
-	const DataOffsetByteLength = 32
-	const StrLengthByteLength = 32
-	const StrLengthStartPos = 2 + ((FnSelectorByteLength + DataOffsetByteLength) * 2)
-	const StrDataStartPos = 2 + ((FnSelectorByteLength + DataOffsetByteLength + StrLengthByteLength) * 2)
-
-	re := regexp.MustCompile(`/0+$/`)
-	codeString = "0x" + strings.ReplaceAllString(code[138:], "")
-
-	// If the codeString is an odd number of characters, add a trailing 0
-	if len(codeString) % 2 == 1 {
-		codeString += "0"
-	}
-	
-
-	return hexutil.Decode(codeString) //Have to handle decoding error
 }
---
+++ testing/revert-reasons/test/ERC20.test.js
@@ -0,0 +1,119 @@
+const { BN, constants, expectRevert } = require('@openzeppelin/test-helpers');
+const { expect } = require('chai');
+const { ZERO_ADDRESS } = constants;
+
+const ERC20Mock = artifacts.require('ERC20Mock');
+
+contract('ERC20', function (accounts) {
+  const [ initialHolder, recipient] = accounts;
+
+  const name = 'My Token';
+  const symbol = 'MTKN';
+
+  const initialSupply = new BN(100);
+
+  beforeEach(async function () {
+    this.token = await ERC20Mock.new(name, symbol, initialHolder, initialSupply);
+  });
+
+  describe('decrease allowance', function () {
+    describe('when the spender is not the zero address', function () {
+      const spender = recipient;
+
+      function shouldDecreaseApproval (amount) {
+        describe('when there was no approved amount before', function () {
+          it('reverts', async function () {
+            await expectRevert(this.token.decreaseAllowance(
+              spender, amount, { from: initialHolder }), 'ERC20: decreased allowance below zero',
+            );
+          });
+        });
+
+        describe('when the spender had an approved amount', function () {
+          const approvedAmount = amount;
+
+          beforeEach(async function () {
+            ({ logs: this.logs } = await this.token.approve(spender, approvedAmount, { from: initialHolder }));
+          });
+
+          it('reverts when more than the full allowance is removed', async function () {
+            await expectRevert(
+              this.token.decreaseAllowance(spender, approvedAmount.addn(1), { from: initialHolder }),
+              'ERC20: decreased allowance below zero',
+            );
+          });
+        });
+      }
+
+      describe('when the sender has enough balance', function () {
+        const amount = initialSupply;
+
+        shouldDecreaseApproval(amount);
+      });
+
+      describe('when the sender does not have enough balance', function () {
+        const amount = initialSupply.addn(1);
+
+        shouldDecreaseApproval(amount);
+      });
+    });
+
+    describe('when the spender is the zero address', function () {
+      const amount = initialSupply;
+      const spender = ZERO_ADDRESS;
+
+      it('reverts', async function () {
+        await expectRevert(this.token.decreaseAllowance(
+          spender, amount, { from: initialHolder }), 'ERC20: decreased allowance below zero',
+        );
+      });
+    });
+  });
+
+  describe('_mint', function () {
+    const amount = new BN(50);
+    it('rejects a null account', async function () {
+      await expectRevert(
+        this.token.mint(ZERO_ADDRESS, amount), 'ERC20: mint to the zero address',
+      );
+    });
+  });
+
+  describe('_burn', function () {
+    it('rejects a null account', async function () {
+      await expectRevert(this.token.burn(ZERO_ADDRESS, new BN(1)),
+        'ERC20: burn from the zero address');
+    });
+
+    describe('for a non zero account', function () {
+      it('rejects burning more than balance', async function () {
+        await expectRevert(this.token.burn(
+          initialHolder, initialSupply.addn(1)), 'ERC20: burn amount exceeds balance',
+        );
+      });
+
+    });
+  });
+
+  describe('_transfer', function () {
+
+    describe('when the sender is the zero address', function () {
+      it('reverts', async function () {
+        await expectRevert(this.token.transferInternal(ZERO_ADDRESS, recipient, initialSupply),
+          'ERC20: transfer from the zero address',
+        );
+      });
+    });
+  });
+
+  describe('_approve', function () {
+
+    describe('when the owner is the zero address', function () {
+      it('reverts', async function () {
+        await expectRevert(this.token.approveInternal(ZERO_ADDRESS, recipient, initialSupply),
+          'ERC20: approve from the zero address',
+        );
+      });
+    });
+  });
+});
