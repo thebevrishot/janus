2c35ca894d6588c085cab9a5c8a91f6c58b8e42e
Chase Ripplinger <chase@ripply.io>
2021-04-16 10:07:16 -0500

    Add unit test for net_peerCount

---
+++ pkg/transformer/eth_net_peerCount_test.go
@@ -0,0 +1,62 @@
+package transformer
+
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/qtumproject/janus/pkg/eth"
+	"github.com/qtumproject/janus/pkg/qtum"
+)
+
+func TestPeerCountRequest(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		testDesc := fmt.Sprintf("#%d", i)
+		t.Run(testDesc, func(t *testing.T) {
+			testPeerCountRequest(t, i)
+		})
+	}
+}
+
+func testPeerCountRequest(t *testing.T, clients int) {
+	//preparing the request
+	requestParams := []json.RawMessage{} //net_peerCount has no params
+	request, err := prepareEthRPCRequest(1, requestParams)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	mockedClientDoer := newDoerMappedMock()
+	qtumClient, err := createMockedClient(mockedClientDoer)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	getPeerInfoResponse := []qtum.GetPeerInfoResponse{}
+	for i := 0; i < clients; i++ {
+		getPeerInfoResponse = append(getPeerInfoResponse, qtum.GetPeerInfoResponse{})
+	}
+	err = mockedClientDoer.AddResponse(2, qtum.MethodGetPeerInfo, getPeerInfoResponse)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	proxyEth := ProxyNetPeerCount{qtumClient}
+	got, err := proxyEth.Request(request)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := eth.NetPeerCountResponse(hexutil.EncodeUint64(uint64(clients)))
+	if !reflect.DeepEqual(got, &want) {
+		t.Errorf(
+			"error\ninput: %d\nwant: %s\ngot: %s",
+			clients,
+			want,
+			got,
+		)
+	}
+
+}
